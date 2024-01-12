4dcaf8e54621979088f6b0c2f06b8a6e6ed7eed2
vanhong29 <118118808+vanhong29@users.noreply.github.com>
2024-01-12 17:35:55 +0700

    Update practice03.sql

--- practice03.sql
+++ practice03.sql
@@ -23,3 +23,32 @@ group by extract(month from submit_date), product_id
 order by extract(month from submit_date), product_id
 
 --Ex5
+SELECT sender_id, count(*)
+FROM messages
+WHERE TO_CHAR(sent_date, 'mm-yyyy') = '08-2022'
+group by sender_id
+order by count(*) DESC
+limit 2
+
+--Ex6
+
+select tweet_id
+from tweets
+where length(content) > 15
+
+--Ex7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
