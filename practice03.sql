229bba61ce6d6a2279b9becc0dfca877911942ea
vanhong29 <118118808+vanhong29@users.noreply.github.com>
2024-01-12 11:10:11 +0700

    Update practice03.sql

--- practice03.sql
+++ practice03.sql
@@ -4,3 +4,22 @@ select name
 from students
 where marks >75
 order by right(name, 3), id
+
+--Ex2
+
+select user_id, concat(upper(left(name, 1)) , lower(right(name, length(name) - 1))) as name
+from users
+
+--Ex3
+SELECT manufacturer, '$' || floor(round(sum(total_sales),-6)/1000000) || ' million' as sales_mil
+FROM pharmacy_sales
+group by manufacturer
+order by floor(round(sum(total_sales),-6)/1000000) DESC, manufacturer desc
+
+--Ex4
+SELECT extract(month from submit_date), product_id as product, ROUND(AVG(stars),2)
+FROM reviews
+group by extract(month from submit_date), product_id
+order by extract(month from submit_date), product_id
+
+--Ex5
