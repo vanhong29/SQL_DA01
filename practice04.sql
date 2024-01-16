49e8a2c7b814f851705a354493e8dc83bcc0ce6b
vanhong29 <118118808+vanhong29@users.noreply.github.com>
2024-01-16 22:43:38 +0700

    Update practice04.sql

--- practice04.sql
+++ practice04.sql
@@ -1,1 +1,66 @@
+--Ex1
+SELECT 
+sum(CASE
+  WHEN device_type = 'laptop' then 1
+  else 0
+END) as laptop_reviews,
+sum(CASE
+  WHEN device_type IN ('tablet','phone') then 1
+  else 0
+END) as mobile_views
+FROM viewership
+
+--Ex2
+select *,
+case
+   when 
+   (x between abs(y-z) AND y+z) AND
+   (y between abs(x-z) AND x + z) AND
+   (z between abs(x-y) AND x + y)
+   then 'Yes'
+   ELSE 'No'
+end as triangle
+from triangle
+
+-- Ex3
+
+  
+
+--Ex4
+SELECT name
+from customer
+where referee_id <> 2 or referee_id is null
+
+-- Ex5
+select pclass, 
+sum(
+case
+    when survived = 1 then 1
+    else 0
+end
+) as survived,
+sum(
+case
+    when survived = 0 then 1
+    else 0
+end
+) as not_survived
+from titanic
+group by pclass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
