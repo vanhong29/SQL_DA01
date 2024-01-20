c6aec03afd213acd18fd890cd64115a41e116665
vanhong29 <118118808+vanhong29@users.noreply.github.com>
2024-01-21 01:01:29 +0700

    Update practice05.sql

--- practice05.sql
+++ practice05.sql
@@ -1,1 +1,13 @@
-
+-- Ex3
+select b.age_bucket,
+round(sum(CASE
+  when a.activity_type = 'send' then time_spent
+  else 0
+END) / sum(a.time_spent) * 100,2)  as send_perc,
+round(sum(CASE
+  when a.activity_type = 'open' then time_spent
+  else 0
+END) / sum(a.time_spent) * 100 ,2) as send_perc
+from activities a join age_breakdown b 
+on a.user_id = b.user_id AND a.activity_type IN ('send','open')
+group by b.age_bucket
