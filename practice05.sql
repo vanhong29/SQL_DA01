f2d1c5da818af40d06d6a081033e5c43622d2cb3
vanhong29 <118118808+vanhong29@users.noreply.github.com>
2024-01-21 21:30:42 +0700

    Update practice05.sql

--- practice05.sql
+++ practice05.sql
@@ -60,6 +60,162 @@ on a.page_id = b.page_id
 group by a.page_id
 having count(b.user_id) = 0
 
+-------------------MID-TERM---------------------
+
+--Ex1
+select distinct replacement_cost
+from film
+order by replacement_cost
+
+--Ex2
+select
+case
+	when replacement_cost between 9.99 AND 19.99 then 'low'
+	when replacement_cost between 20 AND 24.99 then 'medium'
+	when replacement_cost between 25 AND 29.99 then 'high'
+end as replacement_level,
+count(*)
+from film
+group by 
+case
+	when replacement_cost between 9.99 AND 19.99 then 'low'
+	when replacement_cost between 20 AND 24.99 then 'medium'
+	when replacement_cost between 25 AND 29.99 then 'high'
+end
+
+--Ex3
+select a.title, a.length, c.name
+from film a
+join film_category b
+on a.film_id = b.film_id
+join category c
+on b.category_id = c.category_id
+where c.name in ('Drama','Sports')
+order by a.length desc
+
+--Ex4
+select c.name, count(*)
+from film a
+join film_category b
+on a.film_id = b.film_id
+join category c
+on b.category_id = c.category_id
+group by c.name
+order by count(*) desc
+
+--Ex5
+select b.first_name,b.last_name, count(distinct a.film_id)
+from film_actor a
+join actor b
+on a.actor_id = b.actor_id
+group by a.actor_id, b.first_name,b.last_name
+order by count(distinct a.film_id) desc
+
+--Ex6
+
+select count(*)
+from address a
+left join customer b
+on a.address_id = b.address_id
+where b.customer_id is null
+
+--Ex7
+
+select d.city, sum(a.amount)
+from payment a
+join customer b
+on a.customer_id = b.customer_id
+join address c
+on b.address_id = c.address_id
+join city d
+on c.city_id = d.city_id
+group by d.city
+order by sum(a.amount) desc
+
+--Ex8
+
+select concat(d.city || ', '|| e.country), sum(a.amount)
+from payment a
+join customer b
+on a.customer_id = b.customer_id
+join address c
+on b.address_id = c.address_id
+join city d
+on c.city_id = d.city_id
+join country e
+on e.country_id = d.country_id
+group by d.city, e.country
+order by sum(a.amount) desc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
