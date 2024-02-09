099d02f629242d236261a79898f3f7bf5d66bc57
vanhong29 <118118808+vanhong29@users.noreply.github.com>
2024-02-09 11:00:58 +0700

    Update practice07.sql

--- practice07.sql
+++ practice07.sql
@@ -1,1 +1,7 @@
+SELECT extract(year from transaction_date) as year, product_id,
+sum(spend) as curr_year_spend,
+lag(sum(spend)) over(PARTITION BY product_id ORDER BY extract(year from transaction_date)) as prev_year_spend,
+ROUND((sum(spend) - lag(sum(spend)) over(PARTITION BY product_id ORDER BY extract(year from transaction_date)))*100/lag(sum(spend)) over(PARTITION BY product_id ORDER BY extract(year from transaction_date)),2)
+FROM user_transactions
+group by extract(year from transaction_date), product_id
 
